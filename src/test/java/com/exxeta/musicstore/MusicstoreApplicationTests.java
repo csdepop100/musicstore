e2456612043e7c2a4433ca046739823de207cd07
csdepop100 <cs.depop100@gmail.com>
2024-03-21 09:11:10 +0100

    initial commit

---
+++ src/test/java/com/exxeta/musicstore/MusicstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exxeta.musicstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicstoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
